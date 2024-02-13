0a74167f1aab1d28b50bc7cecb5a511cc9b44c27
Nihal Ismail <nihalismailk@gmail.com>
2024-02-13 14:52:53 +0530

    feat: build NSCache for improved performance

--- LinkBucket/Utilities/Managers/NetworkManager.swift
+++ LinkBucket/Utilities/Managers/NetworkManager.swift
@@ -13,15 +13,30 @@ struct NetworkManager{
     
     private init() {}
     
+    private let cache = NSCache<NSString,LPLinkMetadata>()
+    
     func fetchMetadata(url: String, completed: @escaping(LPLinkMetadata?) -> Void){
-        let metadataProvider = LPMetadataProvider()
-        guard let url = URL(string: url) else {return} //TODO: THROW ERROR
-        metadataProvider.startFetchingMetadata(for: url) { metadata, error in
-            if error != nil{
-                //TODO: THROW ERROR
-            }
-            else{
-                completed(metadata)
+        let cacheKey = NSString(string: url)
+        let metadata = cache.object(forKey: cacheKey)
+        if metadata != nil{
+            completed(metadata)
+        }
+        else{
+            guard let url = URL(string: url) else {return} //TODO: THROW ERROR
+            let metadataProvider = LPMetadataProvider()
+            metadataProvider.startFetchingMetadata(for: url) { metadata, error in
+                if error != nil{
+                    //TODO: THROW ERROR
+                }
+                else{
+                    if let metadata{
+                        cache.setObject(metadata, forKey: cacheKey)
+                        completed(metadata)
+                    }
+                    else{
+                        completed(nil)
+                    }
+                }
             }
         }
     }
