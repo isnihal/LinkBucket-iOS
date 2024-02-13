9439aee40a72246325065ed48304dcdb644e5bdf
Nihal Ismail <nihalismailk@gmail.com>
2024-02-13 15:40:52 +0530

    feat: create link model

---
+++ LinkBucket/Models/Link.swift
@@ -0,0 +1,16 @@
+//
+//  Link.swift
+//  LinkBucket
+//
+//  Created by Nihal Ismail on 2/13/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Link{
+    let url: String? = ""
+    
+    init() {}
+}
