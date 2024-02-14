6081a699b2db6cc08021b1e7c51c5e65aa9d6a3b
Nihal Ismail <nihalismailk@gmail.com>
2024-02-14 21:08:35 +0530

    refactor: extract add folder sheet to its own view

--- LinkBucket/View/Screens/FoldersScreen.swift
+++ LinkBucket/View/Screens/FoldersScreen.swift
@@ -9,17 +9,9 @@ import SwiftUI
 import SwiftData
 
 struct FoldersScreen: View {
-    @Environment(\.modelContext) var context
+    
     @Query(sort: \Folder.timestamp) var folders: [Folder]
     @State var showFolderCreationSheet = false
-    @State var inputValue: String = ""
-    
-    private func createFolder(){
-        let folder = Folder(title: inputValue, links: [])
-        context.insert(folder)
-        inputValue = ""
-        showFolderCreationSheet = false
-    }
     
     var body: some View {
         NavigationStack{
@@ -50,40 +42,7 @@ struct FoldersScreen: View {
             .padding()
             .navigationTitle("Your Buckets")
             .sheet(isPresented: $showFolderCreationSheet, content: {
-                VStack{
-                    HStack{
-                        Button(role: .destructive,action: {
-                            showFolderCreationSheet = false
-                        }, label: {
-                            Text("Cancel")
-                        })
-                        Spacer()
-                        Text("Create Bucket")
-                            .font(.title3)
-                            .bold()
-                        Spacer()
-                        Button(action: {
-                            if !inputValue.isEmpty{
-                                createFolder()
-                            }
-                            else{
-                                //TODO: Avoid creating empty folder names
-                            }
-                        }, label: {
-                           Text("Done")
-                        })
-                    }
-                    Spacer()
-                    Image(systemName: "folder.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 240)
-                        .foregroundStyle(.folder)
-                        .padding(.bottom,4)
-                    TextField("Enter the bucket name",text: $inputValue)
-                        .multilineTextAlignment(.center)
-                    Spacer()
-                }
+                AddBucketSheet(showFolderCreationSheet: $showFolderCreationSheet)
                 .padding()
             })
         }
---
+++ LinkBucket/View/Sheets/AddBucketSheet.swift
@@ -0,0 +1,63 @@
+//
+//  AddBucketSheet.swift
+//  LinkBucket
+//
+//  Created by Nihal Ismail on 2/14/24.
+//
+
+import SwiftUI
+
+struct AddBucketSheet: View {
+    @Environment(\.modelContext) var context
+    @Binding var showFolderCreationSheet: Bool
+    @State var inputValue: String = ""
+   
+    
+    private func createFolder(){
+        let folder = Folder(title: inputValue, links: [])
+        context.insert(folder)
+        inputValue = ""
+        showFolderCreationSheet = false
+    }
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Button(role: .destructive,action: {
+                    showFolderCreationSheet = false
+                }, label: {
+                    Text("Cancel")
+                })
+                Spacer()
+                Text("Create Bucket")
+                    .font(.title3)
+                    .bold()
+                Spacer()
+                Button(action: {
+                    if !inputValue.isEmpty{
+                        createFolder()
+                    }
+                    else{
+                        //TODO: Avoid creating empty folder names
+                    }
+                }, label: {
+                   Text("Done")
+                })
+            }
+            Spacer()
+            Image(systemName: "folder.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 240)
+                .foregroundStyle(.folder)
+                .padding(.bottom,4)
+            TextField("Enter the bucket name",text: $inputValue)
+                .multilineTextAlignment(.center)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    AddBucketSheet(showFolderCreationSheet: .constant(false))
+}
